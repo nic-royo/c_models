faecd3d4e87bec73e56c6e06089459bd6adb93ef
nic-royo <nicobregon9@gmail.com>
2024-01-16 11:25:47 +0100

    updates

--- src/random_forest.cpp
+++ src/random_forest.cpp
@@ -4,3 +4,10 @@ int main() {
     std::cout << "RF code will be here" << std::endl;
     return 0;
 }
+
+
+//  mlpack_random_forest --training_file /home/nicolas/Desktop/c_models/data/c_files/training_data.csv --labels_file /home/nicolas/Desktop/c_models/data/c_files/labels_training_data.csv --minimum_leaf_size 20 --num_trees 10 --output_model_file rf_model.bin --print_training_accuracy
+
+// mlpack_random_forest --input_model_file rf_model.bin --test_file /home/nicolas/Desktop/c_models/data/c_files/testing_data.csv --test_labels_file /home/nicolas/Desktop/c_models/data/c_files/labels_testing_data.csv --predictions_file predictions.csv
+
+
